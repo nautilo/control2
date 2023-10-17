3d2da72913088aabda9fdfe17ca3191c8fb34403
Fabisb13 <fabianmunizaga1313@gmail.com>
2023-10-17 03:36:09 -0300

    pagina ingreso, página correo, storage y respuesta secreta

--- src/app/pages/correo/correo.page.ts
+++ src/app/pages/correo/correo.page.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { Router, NavigationExtras } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component, ElementRef, NgZone, OnInit, Output, ViewChild } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
+import { Usuario } from 'src/app/model/usuario';
+
 
 @Component({
   selector: 'app-correo',
@@ -12,9 +15,42 @@ import { IonicModule } from '@ionic/angular';
 })
 export class CorreoPage implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  public usuario: Usuario;
+  
+  constructor(private router: Router, private toastController: ToastController) {
+    this.usuario = new Usuario('', '', '', '', '')
   }
 
+  public ngOnInit(): void {
+  }
+
+  public recuperarContrasena(): void {
+    if (this.usuario) {
+      const mensajeError = this.usuario.validarCorreo();
+      if (mensajeError) {
+        this.mostrarMensaje(mensajeError);
+        return;
+      } else {
+        this.router.navigate(['/incorrecto']);
+      }
+      const usu: Usuario | undefined = this.usuario.buscarUsuarioPorCorreo(this.usuario.correo);
+      
+      if (usu) {
+        const navigationExtras: NavigationExtras = {
+          state: {
+            usuario: usu
+          }
+        };
+        this.router.navigate(['/pregunta'], navigationExtras);
+      }
+    }
+  }
+
+  async mostrarMensaje(mensaje: string, duracion?: number) {
+    const toast = await this.toastController.create({
+        message: mensaje,
+        duration: duracion? duracion: 2000
+      });
+    toast.present();
+  }
 }
