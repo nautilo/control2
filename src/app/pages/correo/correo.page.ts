89a4f80e55fac17b08f4b880ac1c67be73d41ba1
Fabisb13 <fabianmunizaga1313@gmail.com>
2023-10-21 19:10:32 -0300

    Página de correo e ingreso arregladas

--- src/app/pages/correo/correo.page.ts
+++ src/app/pages/correo/correo.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { DataBaseService } from 'src/app/services/data-base.service';
 
 @Component({
   selector: 'app-correo',
@@ -11,10 +12,15 @@ import { IonicModule } from '@ionic/angular';
   imports: [IonicModule, CommonModule, FormsModule]
 })
 export class CorreoPage implements OnInit {
+  public correo: string = 'atorres@duocuc.cl';
 
-  constructor() { }
+  constructor(private bd: DataBaseService) { }
 
   ngOnInit() {
+  }
+
+  recuperarContrasena(){
+    this.bd.leerUsuario(this.correo);
   }
 
 }
--- src/app/pages/ingreso/ingreso.page.ts
+++ src/app/pages/ingreso/ingreso.page.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-ingreso',
@@ -16,13 +17,17 @@ export class IngresoPage implements OnInit {
   correo = 'atorres@duocuc.cl';
   password = '1234';
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit() {
   }
 
   ingresar() {
     this.authService.login(this.correo, this.password);
+  }
+
+  recuperarContrasena(){
+    this.router.navigate(['correo']);
   }
 
 }
