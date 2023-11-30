ea191ae9a1457dcfb4055da74afa679be72c56cb
nautilo <8lapse@gmail.com>
2023-11-30 10:17:40 -0300

    Generación de APK no firmadagit add .

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'registroapp',
+  appId: 'cl.duoc.asistenciaduoc',
+  appName: 'Asistencia Duoc UC',
   webDir: 'www',
   server: {
     androidScheme: 'https'
